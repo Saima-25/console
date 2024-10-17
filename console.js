a2246d3a553964534f37f3647dea8f3d0816f15c
unknown <maima.alive25@gmail.com>
2024-10-17 21:49:42 +0500

    my first commit

---
+++ console.js
@@ -0,0 +1,4 @@
+console.log("Hi Everyone");
+console.log(45);
+console.log(true);
+console.log("saima", 123);
---
+++ console.ts
@@ -0,0 +1,4 @@
+console.log("Hi Everyone");      // string
+console.log(45);               // number
+console.log(true);              // boolean
+console.log("saima", 123)
